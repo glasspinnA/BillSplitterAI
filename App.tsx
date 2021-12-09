e252a64a9a32704e84f446e45d7a04a203bd79c7
Oscar <oscarsandbergso@gmail.com>
2021-12-09 20:32:07 +0100

    change computer

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { ApplicationProvider } from "react-native-ui-kitten/theme/application/applicationProvider.component";
 import { Stack } from "./src/navigation/Stack";
 import * as eva from "@eva-design/eva";
 import { CustomStatusBar } from "./src/component/StatusBar";
+import { ApplicationProvider, Button } from "@ui-kitten/components";
 export default function App() {
   return (
     <ApplicationProvider {...eva} theme={eva.dark}>
       <>
+        <Button>Hello</Button>
         <CustomStatusBar />
         <Stack />
       </>
--- src/component/UserFormComponent.tsx
+++ src/component/UserFormComponent.tsx
@@ -1,12 +1,7 @@
 import * as React from "react";
-import { BaseColor } from "../enums/BaseColor";
-import { FontSize } from "../enums/Text/FontSize";
-import { FontWeight } from "../enums/Text/FontWeight";
 import { useForm, Controller } from "react-hook-form";
-import { Text } from "./baseComponents/Text";
-import { TextInput } from "./baseComponents/TextInput";
-import { Button } from "./baseComponents/Button";
-
+import { Input, Text, Button } from "@ui-kitten/components";
+import { Status } from "./../constant/Status";
 export interface UserFormProps {
   data: t[];
   onUserSubmit: (data: any) => void;
@@ -37,18 +32,12 @@ export function UserForm(props: UserFormProps) {
             required: true,
           }}
           render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              fontWeight={FontWeight.EXTRA_BOLD}
-              fontSize={FontSize.H4}
-              placeholder={x.Placeholder}
-              text={value}
-              onChangeText={onChange}
-            />
+            <Input placeholder={x.Placeholder} value={value} onChangeText={onChange} />
           )}
           name={x.Name}
           defaultValue=""
         />
-        {errors[x.Name] && <Text color={BaseColor.DANGER}>{x.ErrorMessage}</Text>}
+        {errors[x.Name] && <Text status={Status.DANGER}>{x.ErrorMessage}</Text>}
       </>
     );
   });
@@ -61,14 +50,8 @@ export function UserForm(props: UserFormProps) {
   return (
     <>
       {elements}
-      <Button color={BaseColor.PRIMARY} onPress={handleSubmit(onPress)}>
-        Hello
-      </Button>
-      {props.canProcced && (
-        <Button color={BaseColor.PRIMARY} onPress={null}>
-          Hello
-        </Button>
-      )}
+      <Button onPress={handleSubmit(onPress)}>Hello</Button>
+      {props.canProcced && <Button onPress={() => null}>Hello</Button>}
     </>
   );
 }
