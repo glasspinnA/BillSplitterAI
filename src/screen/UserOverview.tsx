4636753ced465bae768f078a627b493f0c637602
glasspinna <glasspinnar@gmail.com>
2021-12-27 21:02:34 +0100

    work

--- src/screen/UserOverview.tsx
+++ src/screen/UserOverview.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
-import { ListRenderItemInfo } from "react-native";
-import { Flatlist } from "../component/baseComponents/Flatlist";
+import { ListRenderItemInfo, View } from "react-native";
 import { ScreenContainer } from "../component/ScreenContainer";
 import { ScreenName } from "../constant/ScreenName";
-import { OverviewComponent } from "./OverviewComponent";
+import { INavigateActionButton, OverviewComponent } from "./OverviewComponent";
 import User from "../interfaces/User/IUser";
 import { FlatListItem, FlexDirection } from "../styles/SharedStyles";
 import { Text } from "@ui-kitten/components";
 import { useAppContext } from "../context/Consumer";
+import { Flatlist } from "../component/baseComponents/Flatlist";
+import { ItemRow } from "../component/ItemRow";
+import { GetItemData } from "../helpers/MappingHelper";
 
 export interface UserOverviewProps {}
 
@@ -16,16 +18,19 @@ export function UserOverviewScreen(props: UserOverviewProps) {
 
   const renderItem = ({ item }: ListRenderItemInfo<User>) => {
     return (
-      <FlatListItem.Item>
-        <FlatListItem.Row flexDirection={FlexDirection.ROW}>
-          <FlatListItem.Column flex={2}>
-            <Text>{item.Name}</Text>
-          </FlatListItem.Column>
-          <FlatListItem.Column>
-            <Text>{item.Income}</Text>
-          </FlatListItem.Column>
-        </FlatListItem.Row>
-      </FlatListItem.Item>
+      // <View style={{ flex: 1 }}>
+      //   <FlatListItem.Item>
+      //     <FlatListItem.Row flexDirection={FlexDirection.ROW}>
+      //       <FlatListItem.Column flex={2}>
+      //         <Text>{item.Name}</Text>
+      //       </FlatListItem.Column>
+      //       <FlatListItem.Column>
+      //         <Text>{item.Income}</Text>
+      //       </FlatListItem.Column>
+      //     </FlatListItem.Row>
+      //   </FlatListItem.Item>
+      // </View>
+      <ItemRow item={GetItemData(item.Name, item.Income)} />
     );
   };
   const getFlatList = (): JSX.Element => (
@@ -33,6 +38,13 @@ export function UserOverviewScreen(props: UserOverviewProps) {
   );
   const getOverviewTitle = (): string => "User\nOverview";
   const getButtonTitle = (): string => "Add User";
+  const getNavigationButton = (): INavigateActionButton => {
+    return {
+      title: "Next",
+      screenToNavigate: ScreenName.BILLING_OVERVIEW,
+    };
+  };
+
   return (
     <ScreenContainer>
       <OverviewComponent
@@ -40,6 +52,7 @@ export function UserOverviewScreen(props: UserOverviewProps) {
         buttonTitle={getButtonTitle()}
         overviewTitle={getOverviewTitle()}
         flatlist={getFlatList()}
+        navigateButtonAction={getNavigationButton()}
       />
     </ScreenContainer>
   );
